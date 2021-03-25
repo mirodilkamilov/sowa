3aeb4d23e5ab6ad8be65acceb5ad235b3222d6b4
Mirodil <mirodilkamilov1999@gmail.com>
2021-03-25 16:49:08 +0500

    setLocale in every request

--- app/Http/Middleware/DashboardSetLocale.php
+++ app/Http/Middleware/DashboardSetLocale.php
@@ -16,15 +16,25 @@ class DashboardSetLocale
      */
     public function handle(Request $request, Closure $next)
     {
-        if ($request->has('change-language')) {
-            $locale = $request->query('change-language');
-
-            if ($this->isValidLanguageQuery($locale)) {
-                app()->setLocale($locale);
-                $request->session()->put('language', $locale);
-            }
+        $langInSession = $request->session()->get('language');
+        $locale = $langInSession;
+        if (!isset($langInSession)) {
+            $locale = config('app.locale');
+            $request->session()->put('language', $locale);
         }
 
+
+        if ($request->has('change-language')) {
+            $langFromQuery = $request->query('change-language');
+
+            if ($this->isValidLanguageQuery($langFromQuery)) {
+                $locale = $langFromQuery;
+                $request->session()->put('language', $locale);
+            }
+
+        }
+
+        app()->setLocale($locale);
         return $next($request);
     }
 
