06e710c65cb633b58f42da1516197e0af2bef4a4
Mirodil <mirodilkamilov1999@gmail.com>
2021-04-30 14:22:45 +0500

    category-add modal made component

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -56,50 +56,7 @@
     </div>
     <!-- END: Content-->
 
-    <!-- Add Modal -->
-    <div class="modal fade" id="category-add" tabindex="-1"
-         role="dialog"
-         aria-labelledby="category-addTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header bg-success white">
-                    <h5 class="modal-title" id="category-addTitle">{{ __('Create a category') }}</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form class="form" action="{{ route('categories.store') }}" method="post" id="category-create">
-                        @csrf
-                        @foreach($availableLangs as $lang)
-                            <div class="col-sm-12 data-field-col pt-2 p-1 mb-0 form-label-group">
-                                <input type="text" class="form-control category-edit-input" id="category_{{$lang}}"
-                                       name="category[{{ $lang }}]"
-                                       placeholder="{{ __('Category') . " ({$lang})" }}"
-                                       value="{{ old("category.{$lang}") }}">
-                                <label
-                                    for="category_{{$lang}}"
-                                    style="padding-left: 0.6rem; top: 0;">{{ __('Category') . " ({$lang})" }}</label>
-                                @error("category.{$lang}")
-                                <p class="text-danger pt-1 mb-0">{{ $message }}</p>
-                                @enderror
-                            </div>
-                        @endforeach
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <div class="add-data-btn">
-                        <button class="btn btn-outline-success mr-1" type="submit" form="category-create">
-                            Add category
-                        </button>
-                    </div>
-                    <div class="cancel-data-btn">
-                        <button class="btn btn-outline-primary" data-dismiss="modal">Cancel</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <x-dashboard.create-category-modal :availableLangs="$availableLangs"/>
 
     <!-- Delete Modal -->
     <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
@@ -156,14 +113,5 @@
             $("#category-title").text(modalMessage);
         });
     </script>
-
-    @if($errors->any())
-        @push('category-modal-show')
-            <script>
-                $('#category-add').modal('show');
-            </script>
-        @endpush
-    @endif
-
 
 @endsection
--- resources/views/dashboard/projects/create.blade.php
+++ resources/views/dashboard/projects/create.blade.php
@@ -35,9 +35,10 @@
 
                                                         <div class="tab-content pt-2 col-md-12 col-12 pr-0 pl-0">
                                                             @foreach($availableLangs as $lang)
-                                                                <div class="tab-pane @if($loop->first) active @endif tab-pane-{{$lang}}"
-                                                                     id="{{ $lang }}-just" role="tabpanel"
-                                                                     aria-labelledby="{{ $lang }}-tab-justified">
+                                                                <div
+                                                                    class="tab-pane @if($loop->first) active @endif tab-pane-{{$lang}}"
+                                                                    id="{{ $lang }}-just" role="tabpanel"
+                                                                    aria-labelledby="{{ $lang }}-tab-justified">
                                                                     <div class="row">
                                                                         <div class="col-md-6 col-12">
                                                                             <div class="form-label-group">
@@ -204,61 +205,7 @@
                                                 @endunless
                                             </fieldset>
 
-                                            <!-- Create Project category Modal -->
-                                            <div class="modal fade" id="category-add" tabindex="-1"
-                                                 role="dialog"
-                                                 aria-labelledby="category-addTitle" aria-hidden="true">
-                                                <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered"
-                                                     role="document">
-                                                    <div class="modal-content">
-                                                        <div class="modal-header bg-success white">
-                                                            <h5 class="modal-title"
-                                                                id="category-addTitle">{{ __('Create a category') }}</h5>
-                                                            <button type="button" class="close" data-dismiss="modal"
-                                                                    aria-label="Close">
-                                                                <span aria-hidden="true">&times;</span>
-                                                            </button>
-                                                        </div>
-                                                        <div class="modal-body">
-                                                            <form class="form" action="{{ route('categories.store') }}"
-                                                                  method="post" id="category-create">
-                                                                @csrf
-                                                                @foreach($availableLangs as $lang)
-                                                                    <div class="col-sm-12 data-field-col pt-2 p-1 mb-0 form-label-group">
-                                                                        <input type="text"
-                                                                               class="form-control category-edit-input"
-                                                                               id="category_{{$lang}}"
-                                                                               name="category[{{ $lang }}]"
-                                                                               placeholder="{{ __('Category') . " ($lang)" }}"
-                                                                               value="{{ old("category.$lang") }}">
-                                                                        <label
-                                                                            for="category_{{$lang}}"
-                                                                            style="padding-left: 0.6rem; top: 0;">
-                                                                            {{ __('Category') . " ($lang)" }}
-                                                                        </label>
-                                                                        @error("category.$lang")
-                                                                        <p class="text-danger pt-1 mb-0">{{ $message }}</p>
-                                                                        @enderror
-                                                                    </div>
-                                                                @endforeach
-                                                            </form>
-                                                        </div>
-                                                        <div class="modal-footer">
-                                                            <div class="add-data-btn">
-                                                                <button class="btn btn-outline-success mr-1"
-                                                                        type="submit" form="category-create">
-                                                                    Add category
-                                                                </button>
-                                                            </div>
-                                                            <div class="cancel-data-btn">
-                                                                <button class="btn btn-outline-primary"
-                                                                        data-dismiss="modal">Cancel
-                                                                </button>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
+                                            <x-dashboard.create-category-modal :availableLangs="$availableLangs"/>
 
                                             <!-- Step 2 -->
                                             <h6>Step 2</h6>
@@ -357,14 +304,6 @@
             @endif
         </script>
     @endpush
-
-    @if($errors->hasAny('category.*'))
-        @push('category-modal-show')
-            <script>
-                $('#category-add').modal('show');
-            </script>
-        @endpush
-    @endif
 
     @push('category-modal-show-select-change')
         <script>
